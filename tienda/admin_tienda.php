18ba9c719b8aabbc5eb8ce5626aec603cd2287be
iLudWing <43395812+LudWIng12@users.noreply.github.com>
2022-12-08 18:20:47 -0600

    Cambio el css a la carpeta

--- tienda/admin_tienda.php
+++ tienda/admin_tienda.php
@@ -12,7 +12,7 @@
     <title>Document</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="../css/estilos_tienda.css">
-    <link rel="stylesheet" href="../HeadFooter.css">
+    <link rel="stylesheet" href="../css/HeadFooter.css">
 
 </head>
 
